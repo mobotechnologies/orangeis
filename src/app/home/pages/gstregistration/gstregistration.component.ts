be7a10015756898024962a2b013775c5b2b38588
Arunachalam <binary2quantum001@DESKTOP-RT9G0OP.B2Q.local>
2020-10-26 16:28:38 +0530

    26/10/2020 gitregistration and digitasignature

--- src/app/home/pages/gstregistration/gstregistration.component.ts
+++ src/app/home/pages/gstregistration/gstregistration.component.ts
@@ -29,6 +29,14 @@ export class GstregistrationComponent implements OnInit {
   imgsrc3: any;
   imgsrc4: any;
   imgsrc5: any;
+  imgsrc6: any;
+  imgsrc7: any;
+  imgsrc8: any;
+  payKit: any;
+  imgsrc9: any;
+  selectedEntity: any;
+  dtype: string;
+  renttype: string;
 
 
   constructor(private localeService: BsLocaleService,private spinner: NgxSpinnerService, private cookieService: CookieService, private _countrymodel: CountryService, private router: Router, private route: ActivatedRoute, private location: Location, private fb: FormBuilder, private _validation: ValidationService,private coperate:CooperateserviceService) {
@@ -46,6 +54,9 @@ export class GstregistrationComponent implements OnInit {
    }
 
   ngOnInit() {
+    this.dtype="Proprietor";
+    this.renttype="if not own place";
+    $(".direc").hide();
   }
   next()
   {
@@ -81,20 +92,22 @@ export class GstregistrationComponent implements OnInit {
     firmname: ['', [Validators.required]],
     businessentity: ['', [Validators.required]],
     businessaddress: ['', [Validators.required]],
+    secondaryaddress: [''],
     goodsservice: ['', [Validators.required]],
     businessactivity: ['', [Validators.required]],
     commencement: ['', [Validators.required]],
-    mobileno: ['', [Validators.required]],
-    emailid: ['', [Validators.required]],
+    mobileno: ['', [Validators.required,ValidationService.numberValidator]],
+    emailid: ['', [Validators.required,ValidationService.emailValidator]],
     compositionscheme: ['', [Validators.required]],
     Photograph:this.fb.array([this.initnewRows1()]),
     identityproof:this.fb.array([this.initnewRows2()]),
     pancard:this.fb.array([this.initnewRows3()]),
     addressproof: ['', [Validators.required]],
     electricitybill: ['', [Validators.required]],
-    rentagreement: ['', [Validators.required]],
+    rentagreement: [''],
     noc: ['', [Validators.required]],
     bankaccountproof: ['', [Validators.required]],
+    companypan: [''],
   });
   initnewRows1(): FormGroup {
     return this.fb.group({
@@ -124,7 +137,7 @@ export class GstregistrationComponent implements OnInit {
     this.formArray2.removeAt(index);
   }
   addNewRow3() {
-    this.formArray3.push(this.initnewRows1());
+    this.formArray3.push(this.initnewRows3());
   }
   deleteRow3(index: number) {
     this.formArray3.removeAt(index);
@@ -145,6 +158,7 @@ export class GstregistrationComponent implements OnInit {
     //  this.imgsrc=event.target.files;
     this.tmp_files.push(event.target.files);
     this.imgsrc1 = event.target.files[0].name;
+    $('.upldtext4').text(event.target.files[0].name);
     var reader = new FileReader();
   
     reader.readAsDataURL(event.target.files[0]); // read file as data url
@@ -157,6 +171,7 @@ export class GstregistrationComponent implements OnInit {
     //  this.imgsrc=event.target.files;
     this.tmp_files.push(event.target.files);
     this.imgsrc2 = event.target.files[0].name;
+    $('.upldtext5').text(event.target.files[0].name);
     var reader = new FileReader();
   
     reader.readAsDataURL(event.target.files[0]); // read file as data url
@@ -169,6 +184,7 @@ export class GstregistrationComponent implements OnInit {
     //  this.imgsrc=event.target.files;
     this.tmp_files.push(event.target.files);
     this.imgsrc3 = event.target.files[0].name;
+    $('.upldtext6').text(event.target.files[0].name);
     var reader = new FileReader();
   
     reader.readAsDataURL(event.target.files[0]); // read file as data url
@@ -181,6 +197,7 @@ export class GstregistrationComponent implements OnInit {
     //  this.imgsrc=event.target.files;
     this.tmp_files.push(event.target.files);
     this.imgsrc4 = event.target.files[0].name;
+    $('.upldtext7').text(event.target.files[0].name);
     var reader = new FileReader();
   
     reader.readAsDataURL(event.target.files[0]); // read file as data url
@@ -193,6 +210,7 @@ export class GstregistrationComponent implements OnInit {
     //  this.imgsrc=event.target.files;
     this.tmp_files.push(event.target.files);
     this.imgsrc5 = event.target.files[0].name;
+    $('.upldtext8').text(event.target.files[0].name);
     var reader = new FileReader();
   
     reader.readAsDataURL(event.target.files[0]); // read file as data url
@@ -201,8 +219,206 @@ export class GstregistrationComponent implements OnInit {
      
     }
   }
+  public proof1= [];
+  public proof2= [];
+  public proof3= [];
+  onSelectedfile6(event, index) {
+    //  this.imgsrc=event.target.files;
+    this.tmp_files.push(event.target.files);
+    this.imgsrc6 = event.target.files[0].name;
+    this.proof3.push(this.imgsrc6);
+    $('.upldtext3'+index).text(event.target.files[0].name);
+    var reader = new FileReader();
+  
+    reader.readAsDataURL(event.target.files[0]); // read file as data url
+    reader.onload = (event) => { // called once readAsDataURL is completed
+      console.log(reader.result);
+     
+    }
+  }
+  onSelectedfile7(event, index) {
+    //  this.imgsrc=event.target.files;
+    this.tmp_files.push(event.target.files);
+    this.imgsrc7 = event.target.files[0].name;
+    this.proof2.push(this.imgsrc7);
+    $('.upldtext2'+index).text(event.target.files[0].name);
+    var reader = new FileReader();
+  
+    reader.readAsDataURL(event.target.files[0]); // read file as data url
+    reader.onload = (event) => { // called once readAsDataURL is completed
+      console.log(reader.result);
+     
+    }
+  }
+  onSelectedfile8(event, index) {
+    //  this.imgsrc=event.target.files;
+    this.tmp_files.push(event.target.files);
+    this.imgsrc8 = event.target.files[0].name;
+    this.proof1.push(this.imgsrc8);
+    $('.uploadtext'+index).text(event.target.files[0].name);
+    var reader = new FileReader();
+  
+    reader.readAsDataURL(event.target.files[0]); // read file as data url
+    reader.onload = (event) => { // called once readAsDataURL is completed
+      console.log(reader.result);
+     
+    }
+  }
+  onSelectedfile9(event, index) {
+    //  this.imgsrc=event.target.files;
+    this.tmp_files.push(event.target.files);
+    this.imgsrc9 = event.target.files[0].name;
+    $('.upldtext9').text(event.target.files[0].name);
+    var reader = new FileReader();
+  
+    reader.readAsDataURL(event.target.files[0]); // read file as data url
+    reader.onload = (event) => { // called once readAsDataURL is completed
+      console.log(reader.result);
+     
+    }
+  }
+  selectChangeBusinessEntity(event:any)
+  {
+    this.selectedEntity = event.target.value;
+    const selectEl = event.target;
+    const val = selectEl.options[selectEl.selectedIndex].getAttribute('data-somedata');
+    const control1 = <FormArray>this.gstregistration.controls['Photograph'];
+ 
+    for(let i = control1.length; i > 0; i--) {
+      
+        control1.removeAt(i)
+    }
+    const control2 = <FormArray>this.gstregistration.controls['identityproof'];
+        for(let i = control2.length; i > 0; i--) {
+            control2.removeAt(i)
+    }
+    const control3 = <FormArray>this.gstregistration.controls['pancard'];
+        for(let i = control3.length; i >0; i--) {
+            control3.removeAt(i)
+    }
+    if(val=="privatelimited" || val=="llp" || val=="trust" )
+    {
+         this.dtype="Director";
+         $(".direc").show();
+         this.renttype="*";
+         this.gstregistration.get('rentagreement').setValidators([Validators.required]);
+         this.gstregistration.get('rentagreement').updateValueAndValidity();
+         this.gstregistration.get('companypan').setValidators([Validators.required]);
+         this.gstregistration.get('companypan').updateValueAndValidity();
+    }
+    else
+    {
+      this.dtype="Proprietor";
+      $(".direc").hide();
+      this.renttype="if not own place";
+      this.gstregistration.get(' rentagreement').clearValidators();
+      this.gstregistration.get(' rentagreement').updateValueAndValidity();
+      this.gstregistration.get('companypan').clearValidators();
+      this.gstregistration.get('compabypan').updateValueAndValidity();
+    }
+   
+  }
+  logofile1(index) {
+    $("#file1" + index).trigger('click');
+  }
+  logofile2(index) {
+    $("#file2" + index).trigger('click');
+  }
+  logofile3(index) {
+    $("#filep" + index).trigger('click');
+  }
+  logofile4() {
+    $("#file4").trigger('click');
+  }
+  logofile5() {
+    $("#file5").trigger('click');
+  }
+  logofile6() {
+    $("#file6").trigger('click');
+  }
+  logofile7() {
+    $("#file7").trigger('click');
+  }
+  logofile8() {
+    $("#file8").trigger('click');
+  }
+  logofile9() {
+    $("#file9").trigger('click');
+  }
   submitandpay()
   {
+    var indate = (new Date(this.gstregistration.value.commencement)).toLocaleDateString();
+    this.gstregistration.markAllAsTouched();
+    if (this.gstregistration.valid) {
+      this.spinner.show();
+      for (let i = 0; i < this.tmp_files.length; i++) {
+        const formData = new FormData();
+        formData.append('Imagefile', this.tmp_files[i][0]);
+        formData.append('dirname', this.gstregistration.value.emailid);
+        this.coperate.gstregfileupload(formData).pipe(retryWhen(errors => errors.pipe(delay(1000), take(10)))).subscribe((res: any) => {
+          if (res.success == false) {
+
+          }
+        });
+      }
     
+      const formData = new FormData();
+      formData.append('ApplicationType', this.gstregistration.value.applicanttype);
+      formData.append('ApplicantFullName',this.gstregistration.value.applicantfullname);
+      formData.append('NameofFirm',this.gstregistration.value.firmname);
+      formData.append('TypeofBusinessEntity',this.gstregistration.value.businessentity);
+      formData.append('BusinessAddress1',this.gstregistration.value.businessaddress);
+      formData.append('BusinessAddress2', this.gstregistration.value.secondaryaddress);
+      formData.append('Goods&Services',this.gstregistration.value.goodsservice);
+      formData.append('BusinessActivity',this.gstregistration.value.businessactivity);
+      formData.append('commencementDate',indate);
+      formData.append('compositionscheme',this.gstregistration.value.compositionscheme);
+      formData.append('MobileNumber',this.gstregistration.value.mobileno);
+      formData.append('EmailID', this.gstregistration.value.emailid);
+      formData.append('Photograph',JSON.stringify(this.proof1));
+      formData.append('IdentityProof',JSON.stringify(this.proof2));
+      formData.append('Pancard',JSON.stringify(this.proof3));
+      formData.append('AddressproofBusiness',this.imgsrc1);
+      formData.append('Electricitybill', this.imgsrc2);
+      formData.append('RentAgreement',this.imgsrc3);
+      formData.append('Noc',this.imgsrc4);
+      formData.append('BankProof',this.imgsrc5);
+      formData.append('CompanyPan',this.imgsrc9);
+      formData.append('userId',"0");
+      formData.append('userName', this.gstregistration.value.firmname);
+      formData.append('userEmail', this.gstregistration.value.emailid);
+      formData.append('userMobile', this.gstregistration.value.mobileno);
+      formData.append('payAmount',"4500");
+      formData.append('purpose', "gstregistration");
+      this.coperate.gstregistration(formData).subscribe(
+        apiResponse => {
+          console.log(this.payKit = apiResponse);
+          // console.log(this.payKit.success);
+          if (this.payKit.success == true) {
+            //console.log('dd');
+            this.spinner.hide();
+            console.log(this.payKit.payment_request.longurl);
+            window.location.href = this.payKit.payment_request.longurl;
+          }
+          else {
+            this.spinner.hide();
+            Swal.fire({
+              title: 'Oops !',
+              titleText: 'Payment failed!Please contact famposo for more info ',
+              width: 600,
+              allowOutsideClick: false,
+              confirmButtonText: 'ok'
+            }).then((result) => {
+              if (result.value) {
+                this.router.navigate(['/contactus']);
+              }
+            })
+            console.log('Payment Error');
+          }
+  
+  
+        });
+    }
+   
   }
 }
